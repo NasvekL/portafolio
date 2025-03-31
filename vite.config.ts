9015a3073c602cb40721ee3450304a64d7e6edff
Nahuel Lopez <nahuel.lopez.areco@fing.edu.uy>
2025-03-31 20:23:18 -0300

    nos pasamo a render

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,5 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [tailwindcss()],
-  preview: {
-    host: '0.0.0.0',
-    port: Number(process.env.PORT)|| 8080,
-    allowedHosts: ['healthcheck.railway.app'],
-  },
+  plugins: [tailwindcss()]
 })
