3a6689b6a01be89e50e746ed60463b63ad50b608
Nahuel Lopez <nahuel.lopez.areco@fing.edu.uy>
2025-03-31 20:28:32 -0300

    una vez mas

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,10 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [tailwindcss()]
+  plugins: [tailwindcss()],
+  preview: {
+    host: '0.0.0.0',
+    port: Number(process.env.PORT)|| 8080,
+    allowedHosts: ['healthcheck.railway.app', 'https://portafolio-production-52e5.up.railway.app/'],
+  },
 })
